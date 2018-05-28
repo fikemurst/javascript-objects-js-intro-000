042e5f3ce58e57cbca1ca49e65f9025d1ec33662
michael furst <michael@furst.us>
2018-05-28 18:47:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,6 @@
 var playlist = new Object ({ aTribeCalledQuest: 'can I kick it?' });
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return playlist[aTribeCalledQuest] = 'can I kick it?'
+
+}
