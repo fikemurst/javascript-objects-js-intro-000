2f3117d1328411c3fdd9e0e460237b52a953b13f
michael furst <michael@furst.us>
2018-05-28 18:55:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,8 @@ var playlist = new Object ({'Phil Ochs': "Here's to the State of Mississippi"});
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return playlist['Phil Ochs'] = "Here's to the State of Mississippi"
+}
 
+function removeFromPlaylist(playlist, 'Slowdive'){
+  delete playlist.Slowdive;
 }
