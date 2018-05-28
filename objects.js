2ffd4bfc51a7174ef6dcc5f0b5a2749833f1c892
michael furst <michael@furst.us>
2018-05-28 18:51:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = new Object ({Phil Ochs: "Here's to the State of Mississippi"});
+var playlist = new Object ({'Phil Ochs': "Here's to the State of Mississippi"});
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return playlist[artistName] = songTitle
